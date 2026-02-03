35a7be59c7fa985f49f58c83c9b85c957bd2d3bc
Kalle <kalle.makinen@visma.com>
2026-02-03 08:40:17 +0200

    Wrap HomePageClient in Suspense to improve loading experience, adding HomePageSkeleton as a fallback while data is being fetched.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from 'react';
 import { getAllGames, getAllTags } from '@/lib/games-server';
 import { HomePageClient } from '@/components/HomePageClient';
+import { HomePageSkeleton } from '@/components/LoadingSkeleton';
 
 export default function HomePage() {
-  // Get all games on the server side
   const allGames = getAllGames();
   const availableTags = getAllTags(allGames);
 
-  return <HomePageClient allGames={allGames} availableTags={availableTags} />;
+  return (
+    <Suspense fallback={<HomePageSkeleton />}>
+      <HomePageClient allGames={allGames} availableTags={availableTags} />
+    </Suspense>
+  );
 }
